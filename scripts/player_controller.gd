0cca126a48fcd4fbc5999f049b741c866b829633
CrazyDany <konevdanil789@gmail.com>
2025-01-04 22:05:06 +0300

    Secound Commit
    add CHIZAAA and his animations

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,25 +1,15 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+#Чтобы перевести скорость из сабпикселей/кадр в пиксели/секунда умножь числа на 15/4
+@export var max_walk_speed:float = 52.5
+@export var max_run_speed:float = 90
+@export var max_sprint_speed:float = 112.5
+@export var acceleration:float = 5
+@export var deceleration:float = 7.5
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		pass
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	if Input.is_action_just_pressed("ui_left") and is_on_floor():
+		pass
