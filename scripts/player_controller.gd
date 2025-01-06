682a81ca91ad1b207a3bd61ce53dae46ce05c384
CrazyDany <konevdanil789@gmail.com>
2025-01-06 18:47:40 +0300

    Physic First Revolution
    FULLY PHYSIC REWORK
    ->Better Acceleration and Deceleration
    --->Smooth transition between speeds
    --->Removed conflict with speeds
    --->New Deceleration logic system
    ->S-Meter logic (aka P-Meter)
    ->Updated Jump (quadratic function)
    ->Better ducking
    CODE REFACTORING
    ->Refactoring by a functions (for state mashine in future)
    ->A few decompozition stuffs

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -1,148 +1,159 @@
 extends CharacterBody2D
 
-#Чтобы перевести скорость из сабпикселей/кадр в пиксели/секунда умножь числа на 15/4
-@export var max_walk_speed: float = 105
-@export var walk_accel: float = 210.5
-@export var walk_deccel: float = 210.5
+# ЧТОБЫ КОНВЕРТИРОВАТЬ СКОРОСТЬ ИЗ SMW В ТУ КОТОРУЮ ИСПОЛЬЗУЕТ ДВИЖОК УМНОЖЬТЕ ЕЕ НА 3.75 (Но для ускорений дополнительно поделить на 2.5)
+@export var walk_max_speed: float = 52.5
+@export var walk_accel: float = 90
+@export var walk_deccel: float = 300
 
-@export var max_run_speed: float = 200
-@export var run_accel: float = 125.5
-@export var run_deccel: float = 165
+@export var max_jump_height: float = 45
+@export var min_jump_height: float = 30
 
-@export var max_duck_speed: float = 90
-@export var duck_accel: float = 0
-@export var duck_dessel: float = 65
+#Перменные буфферизации прыжкка
+const buffer_time: float = 0.02
 
-@export var jump_strenth: float = 275
-#Иницилизация дочерних нод
-@onready var sprite = $Sprite
+@export var run_max_speed: float = 120
+@export var run_accel: float = 175
+@export var run_deccel: float = 300
+
+@export var srun_max_speed: float = 225
+@export var srun_accel: float = 225
+@export var srun_deccel: float = 400
+
+@export var smeter_add: float = 0.25
+@export var smeter_sub: float = 0.5
+
+@export var duck_max_speed: float = 0
+@export var duck_deccel: float = 45
+
+var s_meter: float = 0
+
+var leftKey: bool = false
+var rightKey: bool = false
+var upKey: bool = false
+var downKey: bool = false
+var jumpKey: bool = false
+var jumpKey_pressed: bool = false
+var runKey: bool = false
+
+var moveDirectionX = 0
+
+@onready var sprite: AnimatedSprite2D = $Sprite
 @onready var collision = $CollisionShape2D
-@onready var anim:AnimationPlayer = sprite.get_child(0)
-
-#func _ready():
-	#print(anim)
-
-var is_running = false
+@onready var anim: AnimationPlayer = sprite.get_child(0)
+@onready var jump_buffer_timer: Timer = $JumpBuffer
 
 func _physics_process(delta: float) -> void:
-	var input_vector: Vector2 = Vector2.ZERO
-	var is_running:float = Input.is_action_pressed("Run")
-	var is_moving = Input.is_action_pressed("Left") or Input.is_action_pressed("Right")
-	var in_air = not(is_on_floor())
-	var is_ducking:bool = Input.is_action_pressed("Down")
-	var is_looking_up:bool = Input.is_action_pressed("Up")
+	get_input()
 
-	if Input.is_action_pressed("Right"):
-		input_vector.x += 1
-	if Input.is_action_pressed("Left"):
-		input_vector.x -= 1
+	gravity(delta)
+	horizontal_move(delta)
+	using_s_meter()
+	jump()
 
-	if not is_ducking:
-		if input_vector.x != 0:
-			if is_running:
-				accelerate(input_vector.x * run_accel, max_run_speed, delta)
-			else:
-				accelerate(input_vector.x * walk_accel, max_walk_speed, delta)
-		else:
-			if is_running:
-				deccelerate(run_deccel, 0, delta)
-			else:
-				deccelerate(walk_deccel, 0, delta)
-		if not is_running:
-			if is_looking_up:
-				pass
-	else:
-		deccelerate(duck_dessel, 0, delta)
-			
-#	DO YOU BELIVE IN GRAVITY
-	if not(is_on_floor()):
-		velocity.y += get_gravity().y
-		
-#	Прыжок
-	if not is_ducking:
-		if Input.is_action_just_pressed("Jump") and is_on_floor():
-			if not is_running:
-				velocity.y = -jump_strenth
-			else:
-				if velocity.x > 0:
-					velocity.y = -jump_strenth - velocity.x/3
-				elif velocity.x < 0:
-					velocity.y = -jump_strenth + velocity.x/3
-		if Input.is_action_just_released("Jump"):
-			if velocity.y < -50:
-				velocity.y = -50
+	handle_animator()
 
-	if (abs(velocity).x > 0) and (is_moving):
-		anim.speed_scale = (abs(velocity.x) * (max_walk_speed/16))/250
-	else:
-		anim.speed_scale = 1
 	move_and_slide()
-	
-#	Анимация
-	if in_air:
-		if velocity.y > 0:
-			if abs(velocity.x) >= 190:
-				handle_set_animation("RunJump")
-			else:
-				handle_set_animation("Fall")
-		elif not(anim.current_animation == "Fall"):
-			if abs(velocity.x) >= 190:
-				handle_set_animation("RunJump")
-			else:
-				handle_set_animation("Jump")
-	else:
-		if not is_ducking:
-			if is_moving:
-				if (Input.is_action_pressed("Right") and velocity.x < 0) or (Input.is_action_pressed("Left") and velocity.x > 0):
-					handle_set_animation("Skid")
-				else:
-					if abs(velocity.x) >= 190:
-						handle_set_animation("Run")
-					else:
-						handle_set_animation("Walk")
-			else:
-				if not is_looking_up:
-					handle_set_animation("Idle")
-				else:
-					handle_set_animation("Look_Up")
-		else:
-			handle_set_animation("Duck")
 
-			
-#	ЙОУ КРУТЫЕ ПОВОРОТЫ СПРАЙТА
-	if velocity.x < 0 and not sprite.flip_h:
-		sprite.flip_h = true
-	elif velocity.x > 0 and sprite.flip_h:
-		sprite.flip_h = false
-		
-#	Изменение колизии при приседе
-	if is_ducking:
-		if not(collision.scale.y == 0.5):
-			collision.scale.y = 0.5
-			collision.position.y += 3
-	else:
-		if not(collision.scale.y == 1):
-			collision.scale.y = 1
-			collision.position.y -= 3
-		
-func accelerate(accel: float, max_speed: float, delta: float):
-	velocity.x += accel * delta
+func get_input():
+	leftKey = Input.is_action_pressed("Left")
+	rightKey = Input.is_action_pressed("Right")
+	upKey = Input.is_action_pressed("Up")
+	downKey = Input.is_action_pressed("Down")
+	jumpKey_pressed = Input.is_action_just_pressed("Jump")
+	jumpKey = Input.is_action_pressed("Jump")
+	runKey = Input.is_action_pressed("Run")
 
-	if abs(velocity.x) >= max_speed:
-		deccelerate(run_deccel if is_running else walk_deccel, max_speed, delta)
-
-func deccelerate(deccel: float, min_speed: float, delta: float):
-	if velocity.x > min_speed:
-		velocity.x -= deccel * delta
-		if velocity.x < min_speed:
-			velocity.x = min_speed
-	elif velocity.x < min_speed:
-		velocity.x += deccel * delta
-		if velocity.x > min_speed:
-			velocity.x = min_speed
-			
-func handle_set_animation(name:StringName):
+func set_animation(name: StringName):
 	if anim.current_animation == name:
 		return
 	else:
 		anim.play(name)
+
+func handle_animator():
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
+
+	if !(velocity.x == 0) and is_on_floor():
+		if not downKey:
+			if rightKey:
+				if velocity.x > 0:
+					if s_meter == 100:
+						set_animation("Run")
+					else:
+						set_animation("Walk")
+				else:
+					set_animation("Skid")
+			elif leftKey:
+				if velocity.x < 0:
+					if s_meter == 100:
+						set_animation("Run")
+					else:
+						set_animation("Walk")
+				else:
+					set_animation("Skid")
+		else:
+			set_animation("Duck")
+	else:
+		if is_on_floor():
+			if not downKey:
+				if upKey:
+					set_animation("Look_Up")
+				else:
+					set_animation("Idle")
+			else:
+				set_animation("Duck")
+		else:
+			if velocity.y < 0:
+				if s_meter == 100:
+					set_animation("RunJump")
+				else:
+					set_animation("Jump")
+			else:
+				set_animation("Fall")
+				
+	if (abs(velocity).x > 0):
+		anim.speed_scale = (abs(velocity.x) * (walk_max_speed / 16)) / 250
+	else:
+		anim.speed_scale = 1
+
+func gravity(delta: float):
+	if (!is_on_floor()):
+		velocity.y += get_gravity().y * delta
+
+func horizontal_move(delta: float):
+	if not downKey:
+		moveDirectionX = Input.get_axis("Left", "Right")
+		if runKey:
+			if moveDirectionX != 0:
+				velocity.x = move_toward(velocity.x, moveDirectionX * run_max_speed, run_accel * delta)
+				if s_meter == 100:
+					velocity.x = move_toward(velocity.x, moveDirectionX * srun_max_speed, srun_accel * delta)
+			else:
+				velocity.x = move_toward(velocity.x, moveDirectionX * run_max_speed, run_deccel * delta)
+				if s_meter == 100:
+					velocity.x = move_toward(velocity.x, moveDirectionX * srun_max_speed, srun_deccel * delta)
+		else:
+			if moveDirectionX != 0:
+				velocity.x = move_toward(velocity.x, moveDirectionX * walk_max_speed, walk_accel * delta)
+			else:
+				velocity.x = move_toward(velocity.x, moveDirectionX * walk_max_speed, walk_deccel * delta)
+	else:
+		velocity.x = move_toward(velocity.x, moveDirectionX * duck_max_speed, duck_deccel * delta)
+
+func jump():
+	if (jumpKey_pressed) and is_on_floor() and (not downKey):
+		velocity.y = -sqrt(max_jump_height * 2 * get_gravity().y) - abs(velocity.x / 2)
+	if Input.is_action_just_released("Jump"):
+		if velocity.y < -min_jump_height:
+			velocity.y = -min_jump_height
+	if Input.is_action_just_released("Jump"):
+			if velocity.y < -min_jump_height:
+				velocity.y = -min_jump_height
+
+func using_s_meter():
+	if abs(velocity.x) > walk_max_speed:
+		s_meter = clamp(s_meter + smeter_add + s_meter / 500, 0, 100)
+	else:
+		s_meter = clamp(s_meter - smeter_sub - s_meter / 400, 0, 100)
